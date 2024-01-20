cb07dcfceb7857df441e81a2d206a7379d783c11
wujiaqin <458332681@qq.com>
2024-01-20 21:41:58 +0800

    加载配置

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"go_web_app/settings"
+)
 
+func main() {
+	// 1.加载配置
+	if err := settings.Init(); err != nil {
+		fmt.Printf("init settings faild, err:%v\n", err)
+		return
+	}
+	// 2.初始化日志
+	// 3.初始化Mysql连接
+	// 4.初始化Redis连接
+	// 5.注册路由
+	// 6.启动服务
 }
--- settings/settings.go
+++ settings/settings.go
@@ -1,10 +1,74 @@
 package settings
 
-func Init() (err error) {
-	// 指定配置文件名（不需要后缀）
-	viper.setConfigName("config")
-	// 指定配置文件类型
-	viper.setConfigType("yaml")
-	// 指定查找配置文件路径（这里使用相对路径）
+import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
 
+// Conf 定义全局变量，存储配置
+var Conf = new(AppConfig)
+
+type AppConfig struct {
+	Name         string `mapstructure:"name"`
+	Mod          string `mapstructure:"mod"`
+	Port         int    `mapstructure:"port"`
+	Version      string `mapstructure:"version"`
+	*LogConfig   `mapstructure:"log"`
+	*MysqlConfig `mapstructure:"mysql"`
+	*RedisConfig `mapstructure:"redis"`
+}
+
+type LogConfig struct {
+	Level      string `mapstructure:"lever"`
+	FileName   string `mapstructure:"filename"`
+	MaxSize    int    `mapstructure:"max_size"`
+	MaxAge     int    `mapstructure:"max_age"`
+	MaxBackups int    `mapstructure:"max_backups"`
+}
+
+type MysqlConfig struct {
+	Host         string `mapstructure:"host"`
+	Port         int    `mapstructure:"port"`
+	User         string `mapstructure:"user"`
+	Password     string `mapstructure:"password"`
+	DbName       string `mapstructure:"dbname"`
+	MaxOpenConns int    `mapstructure:"max_open_conns"`
+	MaxIdleConns int    `mapstructure:"max_idle_conns"`
+}
+
+type RedisConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Password string `mapstructure:"password"`
+	Db       int    `mapstructure:"db"`
+	PoolSize int    `mapstructure:"pool_size"`
+}
+
+func Init() (err error) {
+	// 指定配置文件路径
+	//viper.SetConfigFile("D:\\GoProject\\go-web-app\\config.yaml")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	// 读取配置
+	if err = viper.ReadInConfig(); err != nil {
+		fmt.Printf("viper.ReadInConfig faild, err:%v\n", err)
+		return
+	}
+	// 把读取到的配置信息反序列化到Conf变量中
+	if err = viper.Unmarshal(Conf); err != nil {
+		fmt.Printf("viper.Unmarshal faild, err:%v\n", err)
+		return
+	}
+	// 监听配置文件
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		fmt.Println("配置文件修改了。。。")
+		if err = viper.Unmarshal(Conf); err != nil {
+			fmt.Printf("viper.Unmarshal faild, err:%v\n", err)
+			return
+		}
+	})
+	return
 }
